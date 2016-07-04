2d0c86cf142c6370791a91f4b78dfcb9645086dc
laozhizi <741393564@qq.com>
2016-07-04 10:59:16 +0800

    test

--- mysql_search.sh
+++ mysql_search.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-a="ac_akb48_vote"
+table_list="table_name_list"
+search_string="search_string"
 
-for i in $a
+for i in $table_list
 do
 	column=`mysql -D system32 -e "desc $i;" | awk '{if($2 ~ /char/ || $2 ~ /text/ || $2 ~ /enum/){print $1}}'`
 	#echo $column
@@ -10,7 +11,7 @@ do
 	then
 		for j in $column
 		do
-			num=`mysql -D system32 -e "select count(1) from $i where $j like '%static.acfun.mm111.net%'" | tail -1`
+			num=`mysql -D system32 -e "select count(1) from $i where $j like '%"$search_string"%'" | tail -1`
 			#echo $num
 			echo $num' '$i:$j
 			sleep 1
